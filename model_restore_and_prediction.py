db1b186b7cc2db32d2d84faa642f300127cf6131
Sharif Noor Zisad <snzisad@localhost.localdomain>
2020-01-22 01:49:35 +0600

    Initial commit

---
+++ model_restore_and_prediction.py
@@ -0,0 +1,143 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+#%matplotlib notebook
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import svm, metrics, datasets
+from sklearn.utils import Bunch
+from sklearn.model_selection import GridSearchCV, train_test_split
+from skimage.io import imread
+from skimage.transform import resize
+import skimage
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from tqdm import tqdm
+from keras.models import Sequential, load_model
+from keras.utils import to_categorical
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint 
+from sklearn.preprocessing import LabelEncoder
+import itertools
+
+
+dataset ='./dataset/' 
+model = load_model('./model/saved_model.h5')
+
+dimension=(48, 48)
+num_channels = 1
+num_batch_size = 32
+
+
+def preprocess_image(file):
+    try:
+        img = skimage.io.imread(file, as_gray = True) # open image into grayscale
+        img =  resize(img, dimension, anti_aliasing=True, mode='reflect') #resige the image of 48*48 size
+        img.shape = img.shape+tuple([num_channels]) #Change the shape from 2-dim to 3-dim
+        
+    except: 
+        return None
+#    img = np.array(img)
+    return img
+    
+
+#Load images in structured directory like it's sklearn sample dataset
+def load_files():
+    
+    images = []
+    target = []
+    
+    image_dir = Path(dataset)
+    folders = [directory for directory in image_dir.iterdir() if directory.is_dir()]
+    categories = [fo.name for fo in folders]
+
+    
+    for i, direc in enumerate(folders):
+        for file in direc.iterdir():
+            img = preprocess_image(file)
+            
+            if img is not None:
+                images.append(img)
+                target.append(categories[i])
+           
+    
+    target = np.array(target)
+    images = np.array(images)
+
+    return Bunch(target=target,
+                 target_names=categories,
+                 images=images)
+    
+
+def evaluate_model():
+    # Evaluating the model on the training and testing set
+    score = model.evaluate(X_train, y_train, verbose=0)
+    print("Training Accuracy: ", score[1]*100)
+    
+    score = model.evaluate(X_test, y_test, verbose=0)
+    print("Testing Accuracy: ", score[1]*100)
+    
+
+def confussion_matrix():
+    # confussion matrix
+    
+    class_predictions = model.predict_classes(X_test, batch_size=num_batch_size, verbose=0)
+    class_names = image_dataset.target_names
+    print(classification_report(np.argmax(y_test, axis=1), class_predictions, target_names = class_names))
+    
+    cm = confusion_matrix(np.argmax(y_test, axis=1), class_predictions)
+    for i in range(0, len(cm)):
+      print(cm[i])
+      
+    
+    plt.figure(figsize=(9,9))
+    plt.imshow(cm, interpolation='nearest', cmap = plt.cm.Blues) #cmap = plt.cm.Greens
+    plt.title("Confussion Matrix")
+    # plt.colorbar()
+    
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation = 45)
+    # plt.yticks(tick_marks, class_names)
+    
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, cm[i, j], 
+               horizontalalignment = 'center',
+               color = 'red' if i==j else 'black')
+      
+    
+    plt.tight_layout()
+    plt.ylabel("True Label")
+    plt.xlabel("Predicted Label")
+    
+    
+
+
+def print_prediction(file_name):
+    prediction_feature = preprocess_image(file_name) 
+    prediction_feature = prediction_feature.reshape(((1,)+dimension+tuple([num_channels])))
+
+    predicted_vector = model.predict_classes(prediction_feature)
+    predicted_class = le.inverse_transform(predicted_vector) 
+    print("The predicted class is:", predicted_class[0], '\n') 
+    
+
+    
+image_dataset = load_files()
+
+#Encode all level
+le = LabelEncoder()
+yy = to_categorical(le.fit_transform(image_dataset.target)) 
+num_labels = yy.shape[1]
+images = image_dataset.images
+
+#Split data
+X_train, X_test, y_train, y_test = train_test_split(images, yy, test_size=0.3,random_state=109)
+
+
+
+evaluate_model()
+confussion_matrix()
+print_prediction('test/Aaron_Peirsol_0002.jpg')
+
